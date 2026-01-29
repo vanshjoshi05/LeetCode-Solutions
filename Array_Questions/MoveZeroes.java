b67cf190230ed315de0a173603cdf0077843330f
vanshjoshi05 <vjvanshjoshi06@gmail.com>
2026-01-29 22:08:10 +0530

    Added Solution For Move Zeroes To End

---
+++ Array_Questions/MoveZeroes.java
@@ -0,0 +1,17 @@
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int i =0;
+        
+        
+        for (int j=0;j<nums.length;j++){
+            if (nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+        }
+    }
+}
+    
+

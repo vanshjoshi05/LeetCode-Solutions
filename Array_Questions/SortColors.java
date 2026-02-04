beb418e744f663b232595da7383d1a7d7fa8b31e
vanshjoshi05 <vjvanshjoshi06@gmail.com>
2026-02-04 23:22:16 +0530

    Added solution fro Sort Colors

---
+++ Array_Questions/SortColors.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class SortColors {
+    
+
+    public void sortColors(int[] nums) {
+        int n =nums.length;
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if (nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+    }
+}
+

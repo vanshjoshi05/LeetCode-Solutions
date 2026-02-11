2b573fe1b233bb59761088759c36a0edce877b48
vanshjoshi05 <vjvanshjoshi06@gmail.com>
2026-02-11 23:40:16 +0530

    Added solution for Next Permutation

---
+++ Array_Questions/NextPermutation.java
@@ -0,0 +1,36 @@
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int index=-1;
+        for (int i=n-2;i>=0;i--){
+            if (nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if (index==-1){
+            reverse(nums,0,n-1);
+            return;
+        }
+        for (int i=n-1;i>=index;i--){
+            if (nums[i]>nums[index]){
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+                break;
+            }
+        }
+        reverse(nums,index+1,n-1);
+    }
+    private void reverse(int nums[],int start,int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
+

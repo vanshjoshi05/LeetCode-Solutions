054e3ec51dff477677e68e24f22ac4ef8fb7efad
vanshjoshi05 <vjvanshjoshi06@gmail.com>
2026-02-15 23:47:16 +0530

    Added the solution for Spiral Matrix

---
+++ Array_Questions/spiralMatrix.java
@@ -0,0 +1,38 @@
+public class spiralMatrix {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int left=0;
+        int top=0;
+        int bottom=matrix.length-1;
+        int right=matrix[0].length-1;
+        List <Integer> ls=new ArrayList<>();
+        while (top<=bottom&&left<=right){
+            for (int i =left;i<=right;i++){
+                ls.add(matrix[top][i]);
+            }
+            top++;
+            for (int i=top;i<=bottom;i++){
+                ls.add(matrix[i][right]);
+            }
+            right--;
+            if (top<=bottom){
+                for (int i=right;i>=left;i--){
+                    ls.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left<=right){
+                for (int i=bottom;i>=top;i--){
+                    ls.add(matrix[i][left]);
+                }
+                left++;
+            }
+            
+
+            
+
+        }
+        return ls;
+    }
+}
+
